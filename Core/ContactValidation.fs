b692ccc388b19d733bc0bee0505c313dd9a3dc32
Walaa-osama <152403594+Walaa-osama@users.noreply.github.com>
2024-12-12 16:48:35 +0200

    Update ContactValidation.fs
    Update ContactValidation

--- Core/ContactValidation.fs
+++ Core/ContactValidation.fs
@@ -1,2 +1,16 @@
 ﻿module ContactValidation
 open System.Text.RegularExpressions
+let validatePhoneNumber (phone: string) =
+    let pattern = @"^\d{3}-\d{3}-\d{4}$"  
+    if Regex.IsMatch(phone, pattern) then
+        Ok phone
+    else
+        Error ("Invalid phone number format")
+
+let validateEmail (email: string) =
+    let pattern = @"^[^@]+@[^@]+\.[^@]+$"  
+    if Regex.IsMatch(email, pattern) then
+        Ok email
+    else
+        Error ("Invalid email format")
+
