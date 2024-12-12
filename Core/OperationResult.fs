a8ebc56311312aefc81e6c9e03bf4e5a97261783
Maryam Rabie <130417319+Rum5402@users.noreply.github.com>
2024-12-12 10:07:40 +0200

    Update OperationResult.fs

--- Core/OperationResult.fs
+++ Core/OperationResult.fs
@@ -1,1 +1,9 @@
 ﻿module OperationResult
+type ValidationError = 
+    | InvalidPhoneNumber of string
+    | InvalidEmail of string
+    | ContactNotFound of string
+
+type OperationResult<'T> =
+    | Success of 'T
+    | Failure of ValidationError
