6a4a6b19e372b07e2cb5400b31495feaf2f34adb
Hady_Muhammed <hadyymohammed@gmail.com>
2024-12-13 00:27:02 +0200

    changes to contact repository

--- Repositories/ContactRepository.fs
+++ Repositories/ContactRepository.fs
@@ -1,3 +1,17 @@
 ﻿module ContactRepository
 open Contact
 
+type ContactDatabase = Map<int, Contact>
+let initialDatabase: ContactDatabase = Map.empty
+
+let getAllContacts (contacts: ContactDatabase): ContactDatabase =
+    contacts
+
+let addContact (contact: Contact) (contacts: ContactDatabase): ContactDatabase =
+    contacts |> Map.add contact.Id contact
+
+let updateContact (contact: Contact) (contacts: ContactDatabase): ContactDatabase =
+    contacts |> Map.add contact.Id contact
+
+let deleteContact (id: int) (contacts: ContactDatabase): ContactDatabase =
+    contacts |> Map.remove id
