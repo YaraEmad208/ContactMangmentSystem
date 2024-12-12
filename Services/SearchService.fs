43d7896077c09f2f76c12034900e90af20b3adf2
Yara Emad <104102462+YaraEmad208@users.noreply.github.com>
2024-12-13 00:39:04 +0200

    Update SearchService.fs

--- Services/SearchService.fs
+++ Services/SearchService.fs
@@ -1,3 +1,9 @@
 ﻿module SearchService
 open ContactRepository
 
+let searchContacts (searchTerm: string) (contacts: ContactDatabase) =
+    contacts
+    |> Map.filter (fun _ contact -> 
+        contact.Name.Contains(searchTerm) || 
+        contact.Phone.Contains(searchTerm) || 
+        contact.Email.Contains(searchTerm))
